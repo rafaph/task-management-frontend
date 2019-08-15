a915c9b13bccbe15dff1ebb3398f95689287b03f
Raphael Castro <rafaphcastro@gmail.com>
2019-08-15 00:17:34 -0300

    Updates

---
+++ precache-manifest.a03f2877037787de4c138390af926068.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058d25130db85baa503cb32984949911",
+    "url": "/task-management-frontend/index.html"
+  },
+  {
+    "revision": "9a1f56cdb329c802b289",
+    "url": "/task-management-frontend/static/css/main.2e0643fc.chunk.css"
+  },
+  {
+    "revision": "8297462a87c9f68994cb",
+    "url": "/task-management-frontend/static/js/2.b1714b5d.chunk.js"
+  },
+  {
+    "revision": "9a1f56cdb329c802b289",
+    "url": "/task-management-frontend/static/js/main.13dc27f1.chunk.js"
+  },
+  {
+    "revision": "170cb679b8a6db4918f5",
+    "url": "/task-management-frontend/static/js/runtime~main.961a32b0.js"
+  }
+]);
